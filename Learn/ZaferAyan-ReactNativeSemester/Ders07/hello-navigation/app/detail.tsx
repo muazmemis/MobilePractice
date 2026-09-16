eac131f728f9b9fc62a3f8abf8e740e8c1806906
Muaz MEMİŞ <muazmemis@gmail.com>
2026-09-16 11:52:44 +0300

    ders 7 clean project

--- Learn/ZaferAyan-ReactNativeSemester/Ders07/hello-navigation/app/detail.tsx
+++ Learn/ZaferAyan-ReactNativeSemester/Ders07/hello-navigation/app/detail.tsx
@@ -1,9 +1,11 @@
 import { Text, View } from 'react-native';
 
-export default function Home() {
+const Detail = () => {
   return (
     <View>
-      <Text>Index</Text>
+      <Text>Detail</Text>
     </View>
   );
-}
+};
+
+export default Detail;
