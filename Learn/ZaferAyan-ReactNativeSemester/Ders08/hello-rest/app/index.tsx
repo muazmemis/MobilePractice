abdf270013dee5a970e0cf4e12b24cdc434ad80b
Muaz MEMİŞ <muazmemis@gmail.com>
2026-09-17 21:24:03 +0300

    ders 8 update

--- Learn/ZaferAyan-ReactNativeSemester/Ders08/hello-rest/app/index.tsx
+++ Learn/ZaferAyan-ReactNativeSemester/Ders08/hello-rest/app/index.tsx
@@ -1,9 +1,48 @@
-import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { FlatList, Text, View } from 'react-native';
+
+export type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+  };
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+  };
+};
 
 const Index = () => {
+  const [user, setuser] = useState<User[]>([]);
+  const API_URL = 'https://jsonplaceholder.typicode.com/users';
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => setuser(data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <View>
-      <Text>Index</Text>
+      <Text className="p-4 text-6xl font-bold">Index</Text>
+      <FlatList
+        data={user}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <Link href={`/users/${item.id}`}>
+            <Text className="p-2 text-lg">{item.name}</Text>
+          </Link>
+        )}
+      />
     </View>
   );
 };
---
+++ Learn/ZaferAyan-ReactNativeSemester/Ders08/hello-rest/app/users/[id].tsx
@@ -0,0 +1,35 @@
+import { useLocalSearchParams } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
+import { User } from '..';
+
+const UserDetails = () => {
+  const { id } = useLocalSearchParams<{ id: string }>();
+  const [user, setUser] = useState<User | null>(null);
+
+  const API_URL = `https://jsonplaceholder.typicode.com/users/${id}`;
+  useEffect(() => {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => setUser(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <View>
+      <Text>ID: {user?.id}</Text>
+      <Text>Name: {user?.name}</Text>
+      <Text>Username: {user?.username}</Text>
+      <Text>Email: {user?.email}</Text>
+      <Text>Phone: {user?.phone}</Text>
+      <Text>Website: {user?.website}</Text>
+      <Text>Company: {user?.company?.name}</Text>
+      <Text>
+        Address: {user?.address?.street}, {user?.address?.suite}, {user?.address?.city},{' '}
+        {user?.address?.zipcode}
+      </Text>
+    </View>
+  );
+};
+
+export default UserDetails;
