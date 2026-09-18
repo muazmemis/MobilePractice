517ee82be68df564a1f74fd9aa547bceab68a6de
Muaz MEMİŞ <muazmemis@gmail.com>
2026-09-18 22:53:27 +0300

    ders 8 hello bank clean

--- Learn/ZaferAyan-ReactNativeSemester/Ders08/hello-bank/app/_layout.tsx
+++ Learn/ZaferAyan-ReactNativeSemester/Ders08/hello-bank/app/_layout.tsx
@@ -1,12 +1,7 @@
 import '../global.css';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import { Stack } from 'expo-router';
+import { Tabs } from 'expo-router';
 
 export default function Layout() {
-  return (
-    <SafeAreaProvider>
-      <Stack />
-    </SafeAreaProvider>
-  );
+  return <Tabs />;
 }
--- Learn/ZaferAyan-ReactNativeSemester/Ders08/hello-bank/app/index.tsx
+++ Learn/ZaferAyan-ReactNativeSemester/Ders08/hello-bank/app/index.tsx
@@ -1,29 +1,11 @@
-import { Stack, Link } from 'expo-router';
+import { Text, View } from 'react-native';
 
-import { View } from 'react-native';
-
-import { Button } from '@/components/Button';
-import { Container } from '@/components/Container';
-import { ScreenContent } from '@/components/ScreenContent';
-
-export default function Home() {
+const Index = () => {
   return (
-    <View className={styles.container}>
-      <Stack.Screen options={{ title: 'Home' }} />
-      <Container>
-        <ScreenContent path="app/index.tsx" title="Home"></ScreenContent>
-
-        <View className={styles.buttonWrapper}>
-          <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-            <Button title="Show Details" className="mx-6" />
-          </Link>
-        </View>
-      </Container>
+    <View>
+      <Text>Index</Text>
     </View>
   );
-}
-
-const styles = {
-  container: 'flex flex-1 bg-white',
-  buttonWrapper: 'mx-4',
 };
+
+export default Index;
