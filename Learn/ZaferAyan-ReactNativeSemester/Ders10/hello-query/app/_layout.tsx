61b58ca2388950232e4e8514ea380cb1a818fe85
Muaz MEMİŞ <muazmemis@gmail.com>
2026-09-23 10:25:23 +0300

    ders 10 project ready 2

--- Learn/ZaferAyan-ReactNativeSemester/Ders10/hello-query/app/_layout.tsx
+++ Learn/ZaferAyan-ReactNativeSemester/Ders10/hello-query/app/_layout.tsx
@@ -1,12 +1,17 @@
-import '../global.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import '../global.css';
 
 import { Stack } from 'expo-router';
+
+const queryClient = new QueryClient();
 
 export default function Layout() {
   return (
     <SafeAreaProvider>
-      <Stack />
+      <QueryClientProvider client={queryClient}>
+        <Stack />
+      </QueryClientProvider>
     </SafeAreaProvider>
   );
 }
